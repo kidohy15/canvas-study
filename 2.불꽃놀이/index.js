6c58dbd80fdb0ca9b633c035f2beb9b45886404d
kidohy15 <kidohy307@gmail.com>
2024-01-08 16:47:35 +0900

    불꽃놀이 꼬리 만들

--- 2.불꽃놀이/index.js
+++ 2.불꽃놀이/index.js
@@ -1,11 +1,13 @@
 import CanvasOption from "./js/CanvasOption.js";
 import Particle from "./js/Particle.js";
+import Tail from "./js/Tail.js";
 import { hypotenuse, randomNumBetween } from "./js/utils.js";
 
 class Canvas extends CanvasOption {
   constructor() {
     super();
 
+    this.tails = [];
     this.particles = [];
   }
 
@@ -25,19 +27,27 @@ class Canvas extends CanvasOption {
     this.createParticles();
   }
 
-  createParticles() {
-    const PARTICLE_NUM = 400;
-    const x = randomNumBetween(0, this.canvasWidth);
-    const y = randomNumBetween(0, this.canvasHeight);
-    for (let i = 0; i < PARTICLE_NUM; i++) {
-      const r = randomNumBetween(2,100) * hypotenuse(innerWidth, innerHeight) * 0.0001
-      const angle = Math.PI / 180 * randomNumBetween(0, 360)
-      
-      const vx = r * Math.cos(angle)
-      const vy = r * Math.sin(angle)
+  creaTail() {
+    const x = randomNumBetween(this.canvasWidth * 0.2, this.canvasWidth * 0.8);
+    const vy = this.canvasHeight * randomNumBetween(0.01, 0.015) * -1;
+    const color = "255, 255, 255";
+    this.tails.push(new Tail(x, vy, color));
+  }
 
-      const opacity = randomNumBetween(0.6, 0.9)
-      this.particles.push(new Particle(x, y, vx, vy, opacity));
+  createParticles(x, y, color) {
+    const PARTICLE_NUM = 400;
+    // const x = randomNumBetween(0, this.canvasWidth);
+    // const y = randomNumBetween(0, this.canvasHeight);
+    for (let i = 0; i < PARTICLE_NUM; i++) {
+      const r =
+        randomNumBetween(2, 100) * hypotenuse(innerWidth, innerHeight) * 0.0001;
+      const angle = (Math.PI / 180) * randomNumBetween(0, 360);
+
+      const vx = r * Math.cos(angle);
+      const vy = r * Math.sin(angle);
+
+      const opacity = randomNumBetween(0.6, 0.9);
+      this.particles.push(new Particle(x, y, vx, vy, opacity, color));
     }
   }
 
@@ -51,16 +61,28 @@ class Canvas extends CanvasOption {
       delta = now - then;
       if (delta < this.interval) return;
 
-      this.ctx.fillStyle = this.bgColor + '40'; //#00000010
+      this.ctx.fillStyle = this.bgColor + "40"; //#00000010
       this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
       // this.ctx.fillRect(100, 100, 200, 200);
+
+      if (Math.random() < 0.03) this.creaTail();
+
+      this.tails.forEach((tail, index) => {
+        tail.update();
+        tail.draw();
+
+        if (tail.vy > -0.7) {
+          this.tails.splice(index, 1);
+          this.createParticles(tail.x, tail.y, tail.color);
+        }
+      });
+
       this.particles.forEach((particle, index) => {
         particle.update();
         particle.draw();
 
-        if (particle.opacity < 0) this.particles.splice(index, 1)
+        if (particle.opacity < 0) this.particles.splice(index, 1);
       });
-
 
       then = now - (delta % this.interval);
     };
--- 2.불꽃놀이/js/Particle.js
+++ 2.불꽃놀이/js/Particle.js
@@ -1,34 +1,35 @@
 import CanvasOption from "./CanvasOption.js";
 
 export default class Particle extends CanvasOption {
-  constructor(x, y, vx, vy, opacity) {
-    super()
-    this.x = x
-    this.y = y
-    this.vx = vx
-    this.vy = vy
-    this.opacity = opacity
-    this.gravity = 0.12
-    this.friction = 0.93
+  constructor(x, y, vx, vy, opacity, color) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.opacity = opacity;
+    this.gravity = 0.12;
+    this.friction = 0.93;
+    this.color = color;
   }
-  
+
   update() {
-    this.vy += this.gravity
+    this.vy += this.gravity;
 
-    this.vx *= this.friction
-    this.vy *= this.friction
+    this.vx *= this.friction;
+    this.vy *= this.friction;
 
-    this.x += this.vx
-    this.y += this.vy
+    this.x += this.vx;
+    this.y += this.vy;
 
-    this.opacity -= 0.02
+    this.opacity -= 0.02;
   }
 
   draw() {
-    this.ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`
-    this.ctx.beginPath()
-    this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2)
-    this.ctx.fill()
-    this.ctx.closePath()
+    this.ctx.fillStyle = `rgba(${this.color}, ${this.opacity})`;
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
+    this.ctx.fill();
+    this.ctx.closePath();
   }
 }
